4c3c7fc9e60c16ba01c218ab843516d44a878157
Tsvetan <tsvetan.y.petrov@gmail.com>
2019-01-05 06:05:08 +0200

    Log

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+var db_path = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+console.log('Attempting to connect to', db_path);
+mongoose.connect(db_path);
 
 module.exports = {
     mongoose
